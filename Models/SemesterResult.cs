70b1126a4529c998a219043b2984666dad818b6d
MohammadAsalmUddin <aslamuddin0015@gmail.com>
2021-10-30 20:52:04 +0600

    Result Sheet's Design Updated

--- Models/SemesterResult.cs
+++ Models/SemesterResult.cs
@@ -1,17 +1,26 @@
-﻿namespace ResultManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResultManagement.Models
 {
 
     public class SemesterResult
     {
         public int Id { get; set; }
+
+
+
+        [Display(Name = "Course Name")]
         public string Course_Name { get; set; }
+        [Display(Name = "Course Code")]
         public string Course_Code { get; set; }
         public double Ct1 { get; set; }
         public double Ct2 { get; set; }
         public double Ct3 { get; set; }
         public double Part_A { get; set; }
         public double Part_B { get; set; }
+        [Display(Name = "Grade Point")]
         public double Grade_Point { get; set; }
+        [Display(Name = "Latter Grade")]
         public string Latter_Grade { get; set; }
         public double Attendence { get; set; }
         public double Total_Grade { get; set; }
